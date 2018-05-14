d6c58ead134e5ae6ba6540bc4fa40aa53ef1977b
Andy Laughlin <atl11384@gmail.com>
2018-05-14 16:02:19 +0100

    Calculator add test passing successfully

---
+++ src/main/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class CalculatorTest {
+    Calculator calculator;
+
+    @Before
+    public void before(){
+        calculator = new Calculator(7, 8);
+    }
+
+    @Test
+    public void addNums(){
+        assertEquals(15, calculator.addNums());
+    }
+}
